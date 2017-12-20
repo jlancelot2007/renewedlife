2cf14508d25d4e655d6540b2a94bf6abee8f38e4
Andrew Plater <aplater@myfizz.biz>
2017-12-20 18:53:54 +1100

    completed development and ready to test and push

--- application.js
+++ application.js
@@ -6,21 +6,21 @@ $(function(){
 	(function(app){
 
 		// variable definitions go here
-		var $title = $('#title'),
-			$note = $('#note'),
-			$ul = $('#notesList'),
-			li = '<li><a href="#pgNotesDetail?title=LINK">ID</a></li>',
-			notesHdr = '<li data-role="list-divider">Your Notes</li>',
-			noNotes = '<li id="noNotes">You have no notes</li>';
+		var $title = $('#title');
+		var $note = $('#note');
+		var $ul = $('#notesList');
+		var li = '<li><a href="#pgNotesDetail?title=LINK">ID</a></li>';
+		var notesHdr = '<li data-role="list-divider">Your Notes</li>';
+		var noNotes = '<li id="noNotes">You have no notes</li>';
 
 		app.init = function(){
 			app.bindings();
 			app.checkForStorage();
-		};
+		}
 
 		app.bindings = function(){
 			// set up binding for form
-			$('#btnAddNote').on('touchend', function(e){
+			$('#btnAddNote').bind('touchend', function(e){
 				e.preventDefault();
 				// save the note
 				app.addNote(
@@ -28,31 +28,33 @@ $(function(){
 					$('#note').val()
 				);
 			});
-			$(document).on('touchend', '#notesList a', function(e){
+			$(document).on('touchend','#notesList a', function(e){
 				e.preventDefault();
 				var href = $(this)[0].href.match(/\?.*$/)[0];
 				var title = href.replace(/^\?title=/,'');
 				app.loadNote(title);
 			});
-			$(document).on('touchend', '#btnDelete', function(e){
+
+			$(document).on('touchend','#btnDelete',function(e){
 				e.preventDefault();
 				var key = $(this).data('href');
 				app.deleteNote(key);
 			});
-		};
 
+		}
+		
 		app.loadNote = function(title){
 			// get notes
-			var notes = app.getNotes(),
-				// lookup specific note
-				note = notes[title],
-				page = ['<div data-role="page">',
-							'<div data-role="header" data-add-back-btn="true">',
-								'<h1>Notekeeper</h1>',
-								'<a id="btnDelete" href="" data-href="ID" data-role="button" class="ui-btn-right">Delete</a>',
-							'</div>',
-							'<div role="main" class="ui-content"><h3>TITLE</h3><p>NOTE</p></div>',
-						'</div>'].join('');
+			var notes = app.getNotes();
+			// lookup specific note
+			var note = notes[title];
+			var page = '<div data-role="page" data-url="details" data-add-back-btn="true">\
+							<div data-role="header">\
+								<h1>Notekeeper</h1>\
+								<a id="btnDelete" href="" data-href="ID" data-role="button" class="ui-btn-right">Delete</a>\
+							</div>\
+							<div data-role="content"><h3>TITLE</h3><p>NOTE</p></div>\
+						</div>';
 			var newPage = $(page);
 			//append it to the page container
 			newPage.html(function(index,old){
@@ -60,18 +62,17 @@ $(function(){
 						.replace(/ID/g,title)
 						.replace(/TITLE/g,title
 						.replace(/-/g,' '))
-						.replace(/NOTE/g,note);
+						.replace(/NOTE/g,note)
 			}).appendTo($.mobile.pageContainer);
 			$.mobile.changePage(newPage);
-		};
+		}
 
 		app.addNote = function(title, note){
-			var notes = localStorage['Notekeeper'],
-				notesObj;
-			if (notes === undefined || notes === '') {
-				notesObj = {};
+			var notes = localStorage['Notekeeper'];
+			if (notes == undefined || notes == '') {
+				var notesObj = {};
 			} else {
-				notesObj = JSON.parse(notes);
+				var notesObj = JSON.parse(notes)
 			}
 			notesObj[title.replace(/ /g,'-')] = note;
 			localStorage['Notekeeper'] = JSON.stringify(notesObj);
@@ -80,28 +81,26 @@ $(function(){
 			$title.val('');
 			//update the listview
 			app.displayNotes();
-		};
+		}
 
 		app.getNotes = function(){
 			// get notes
 			var notes = localStorage['Notekeeper'];
 			// convert notes from string to object
-			if(notes) return JSON.parse(notes);
-			return [];
-		};
+			return JSON.parse(notes);
+		}
 
 		app.displayNotes = function(){
 			// get notes
-			var notesObj = app.getNotes(),
-				// create an empty string to contain html
-				html = '',
-				n; // make sure your iterators are properly scoped
+			var notesObj = app.getNotes();
+			// create an empty string to contain html
+			var html = '';
 			// loop over notes
 			for (n in notesObj) {
 				html += li.replace(/ID/g,n.replace(/-/g,' ')).replace(/LINK/g,n);
 			}
 			$ul.html(notesHdr + html).listview('refresh');
-		};
+		}
 
 		app.deleteNote = function(key){
 			// get the notes from localStorage
@@ -114,19 +113,18 @@ $(function(){
 			$.mobile.changePage('notekeeper.html');
 			// restart the storage check
 			app.checkForStorage();
-		};
+		}
 
 		app.checkForStorage = function(){
-			var notes = app.getNotes();
 			// are there existing notes?
-			if (!$.isEmptyObject(notes)) {
+			if (localStorage['Notekeeper']) {
 				// yes there are. pass them off to be displayed
 				app.displayNotes();
 			} else {
 				// nope, just show the placeholder
 				$ul.html(notesHdr + noNotes).listview('refresh');
 			}
-		};
+		}
 
 		app.init();
 
--- js/my-app.js
+++ js/my-app.js
@@ -30,17 +30,7 @@ $(document).ready(function(){
 
 function onDeviceReady(){
 
-initMap() {
-        var marrickville = {lat: -33.9072866, lng: 151.15622299999995};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: marrickville
-        });
-        var marker = new google.maps.Marker({
-          position: marrickville,
-          map: map
-        });
-}
+
     
     // Display Quicknote
     getQuickNote();
